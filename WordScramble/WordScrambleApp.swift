ac206ef7856322d0e131aea82542ab86e34bea7b
YasseenR <yasseenrouni@gmail.com>
2024-07-01 14:37:27 -0400

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Yasseen Rouni on 7/1/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
